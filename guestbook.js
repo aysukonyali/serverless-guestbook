74d5c47b3f40d020c33bfacda8f1d5f3b4b0142b
aysukonyali <42514682+aysukonyali@users.noreply.github.com>
2022-07-22 04:22:27 +0200

    guestbook url is added

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://6cd2238c.eu-de.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
